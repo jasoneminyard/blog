6e3e24a1284c77674930e4d39d40dbee4bbe0e0e
Jason Minyard <jason.e.minyard@gmail.com>
2024-01-11 15:45:18 -0600

    test article validations

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -4,4 +4,13 @@ class ArticleTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save article without required attributes" do
+    article = Article.new
+    assert_not article.save
+    assert_not article.update title: "test_one" 
+    assert_not article.update body: "123456789"
+
+    assert article.update body: "1234567890", status: "public"
+  end
 end
