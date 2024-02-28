f783a431aee4e474ea9f23f7284e6c9e9a8d055a
jasoneminyard <jason.e.minyard@gmail.com>
2024-02-28 16:49:45 -0600

    add devise for authentication and one route bugfix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "articles#index"
+  
 
   resources :articles do
     resources :comments
