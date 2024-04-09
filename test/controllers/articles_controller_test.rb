e610ec31fd608f9699486ec6500a77da9a46a7ae
jasoneminyard <jason.e.minyard@gmail.com>
2024-04-09 16:32:33 -0500

    test index article ==1

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -4,12 +4,20 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  test "articles.count" do
+  test "index if @count == 1" do
     get articles_url
     assert_response :success
-    puts "Article.count =" + Article.count.to_s
-    assert Article.count == 0
-    article = Article.new title: "test one", body: "Now is the time for all good men to come to the aid of their country."
+    Article.last.destroy #i need only one Article to go down the correct path
+    assert Article.count == 1
+    article = Article.new title: "test one", body: "Now is the time for all good men to come to the aid of their country.", status: 'public', author: "Billy Bob"
     assert article.save
+    get '/articles'
+    assert_response :success
+    assert_equal "index", @controller.action_name
+    assert_match "article", @response.body
+  end
+
+  test "#show article"
+    
   end
 end
