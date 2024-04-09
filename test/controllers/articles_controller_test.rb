4d7249d90ba427ecaff88f8f1f94a135d842ed03
jasoneminyard <jason.e.minyard@gmail.com>
2024-04-09 14:30:28 -0500

    working on test for last commit

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -4,4 +4,12 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "articles.count" do
+    get articles_url
+    assert_response :success
+    puts "Article.count =" + Article.count.to_s
+    assert Article.count == 0
+    article = Article.new title: "test one", body: "Now is the time for all good men to come to the aid of their country."
+    assert article.save
+  end
 end
