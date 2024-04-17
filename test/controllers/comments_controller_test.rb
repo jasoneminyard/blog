93511efac3e412cc0a68b4996d6e69b8b9fe3ccd
jasoneminyard <jason.e.minyard@gmail.com>
2024-04-17 17:01:20 -0500

    add controller test for destroy

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -35,4 +35,30 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
     assert_equal "Comment was successfully created.", flash[:notice]
   end
 
+  #destroy
+
+  test "signed in users can #destory their comments" do
+    user = users(:one)
+    article = articles(:one)
+    assert sign_in(user)
+
+    assert_difference("Comment.count") do
+      post article_comments_path(article), params: { 
+        comment:
+          { commenter: user.username,
+            body: "This article is interesting!",
+            status: "public",
+            user_id: user.id
+          } }
+    end
+    
+    assert_response :found
+    assert_redirected_to article_path(article)
+    assert_equal "Comment was successfully created.", flash[:notice]
+
+    assert_difference("Comment.count", -1) do
+      delete article_comment_path(article, Comment.last)
+    end
+  end
+
 end
